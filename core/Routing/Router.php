706ea31081f2ab5d908d6fb36a73f565b78785bf
avolutions <acvogt89@gmail.com>
2019-06-04 22:16:18 +0200

    Add routing classes

---
+++ core/Routing/Router.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * AVOLUTIONS
+ * 
+ * An open source PHP framework.
+ * 
+ * @package		AVOLUTIONS
+ * @author		Alexander Vogt <alexander.vogt@avolutions.de>
+ * @copyright	2019 avolutions (http://avolutions.de)
+ * @license		MIT License (https://opensource.org/licenses/MIT)
+ * @link		http://framework.avolutions.de
+ * @since		Version 1.0.0 * 
+ */
+
+namespace core\Routing;
+?>
