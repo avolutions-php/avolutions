586d7d33b70b04b1389fe79e388cadb1ace94cb0
avolutions <acvogt89@gmail.com>
2019-06-04 22:24:53 +0200

    Create Route class

--- core/Routing/Route.php
+++ core/Routing/Route.php
@@ -13,4 +13,22 @@
  */
 
 namespace core\Routing;
+
+/**
+ * Route class
+ *
+ * A Route object which will be added to the RouteCollection.
+ * The Router class will find the corresponding Route object for 
+ * the current request.  
+ *
+ * @package		AVOLUTIONS
+ * @subpackage	Core
+ * @author		Alexander Vogt <alexander.vogt@avolutions.de>
+ * @link		http://framework.avolutions.de/documentation/route
+ * @since		Version 1.0.0 *
+ */
+class Route 
+{
+	
+}
 ?>
