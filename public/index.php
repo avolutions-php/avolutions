8cb109595068a9faa4efe0fb1d08c608c4cabd6e
avolutions <acvogt89@gmail.com>
2019-06-04 21:40:49 +0200

    Add index file

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * AVOLUTIONS
+ * 
+ * An open source PHP framework.
+ * 
+ * @package		AVOLUTIONS
+ * @author		Alexander Vogt <alexander.vogt@avolutions.de>
+ * @copyright	2019 avolutions (http://avolutions.de)
+ * @license		MIT License (https://opensource.org/licenses/MIT)
+ * @link		http://framework.avolutions.de
+ * @since		Version 1.0.0
+ * 
+ */
+
+/**
+ * Load the bootstrap file * 
+ */
+require_once "../bootstrap.php";
+?>
