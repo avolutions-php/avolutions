4eec4a90329b7032f72e670a005ecdb219db9d3b
avolutions <acvogt89@gmail.com>
2019-06-10 19:31:41 +0200

    Add RouteCollection class

--- core/Routing/RouteCollection.php
+++ core/Routing/RouteCollection.php
@@ -4,13 +4,57 @@
  * 
  * An open source PHP framework.
  * 
- * @package		AVOLUTIONS
  * @author		Alexander Vogt <alexander.vogt@avolutions.de>
  * @copyright	2019 avolutions (http://avolutions.de)
  * @license		MIT License (https://opensource.org/licenses/MIT)
  * @link		http://framework.avolutions.de
- * @since		Version 1.0.0 * 
+ * @since		Version 1.0.0 
  */
 
 namespace core\Routing;
+
+use core\Singleton;
+
+/**
+ * RouteCollection class
+ *
+ * The RouteCollection contains all registered routes (Route objects).
+ * The Router class will search in the RouteCollection for a matching route
+ * for the Request.
+ *
+ * @package		avolutions\core\routing
+ * @author		Alexander Vogt <alexander.vogt@avolutions.de>
+ * @link		http://framework.avolutions.de/documentation/routeCollection
+ * @since		Version 1.0.0
+ */
+class RouteCollection extends Singleton
+{
+	/**
+	 * @var array $Routes TODO
+	 */
+	private $Routes = array();
+	
+	/**
+	 * addRoute
+	 * 
+	 * Adds an given Route object to the RouteCollection.
+	 * 
+	 * @param Route $Route An Route object to add to the collection
+	 */
+	public function addRoute($Route) {
+		$this->Routes[] = $Route;
+	}
+	
+	/**
+	 * getRoutes
+	 * 
+	 * Returns an array with all Routes of the RouteCollection
+	 * 
+	 * @return array An array with all Routes of the RouteCollection
+	 */ 
+	public function getRoutes() {
+		return $this->Routes;
+	}
+	
+}
 ?>
