e8149f223b9bb8bf0144d757ced38f7d6eadded1
avolutions <acvogt89@gmail.com>
2019-09-02 19:26:12 +0200

    Add get method to Config class

--- core/class/Config.php
+++ core/class/Config.php
@@ -16,7 +16,8 @@ namespace core;
 /**
  * Config class
  *
- * TODO
+ * The Config class loads all config files at the bootstrapping and can be used to
+ * get the config values anywhere in the framework/application.
  *
  * @package		core
  * @author		Alexander Vogt <alexander.vogt@avolutions.de>
@@ -25,12 +26,15 @@ namespace core;
 class Config extends AbstractSingleton
 {
 	/**
-	 * @var array $configValues An array containing all configuration values
+	 * @var array $configValues An array containing all loaded configuration values
 	 */
 	private static $configValues = array();
 	
 	/**
-	 * TODO
+	 * initialize
+	 *
+	 * Loads all config values from the config files of core and app. The app configs
+	 * overrides the config values of the core.
 	 */
 	public function initialize() {		
 		$coreConfigFiles = array_map('basename', glob(CORE_CONFIG_PATH.'*.php'));
@@ -55,24 +59,34 @@ class Config extends AbstractSingleton
 	/**
 	 * get
 	 * 
-	 * TODO
+	 * Returns the config value for the given key. The key is seperated by slashes (/).
 	 *
-	 * @param TODO
+	 * @param $key string The key (slash separated) of the config value.
 	 *
-	 * @return TODO
+	 * @return mixed The config value
 	 */
 	public static function get($key) {
-		// TODO
+		$identifier = explode('/', $key);
+		
+		$configValues = self::$configValues;
+			
+		foreach($identifier as $key => $value) {
+			$configValues = $configValues[$value];
+		}
+		
+		return $configValues;
 	}		
 	
 	/**
 	 * loadConfigFile
 	 *
-	 * Loads the given config file and return the content.
+	 * Loads the given config file and return the content (array) or an empty array 
+	 * if the file can not be found.
 	 *
-	 * @param string $configFile Full filename with path of the config file.
+	 * @param string $configFile Complete name including the path of the config file.
 	 * 
-	 * @return array An array with the loaded config values.
+	 * @return array An array with the loaded config values or an empty array if 
+     *				 file can not be found.
 	 */
 	private function loadConfigFile($configFile) {				
 		if(file_exists($configFile)) {	
