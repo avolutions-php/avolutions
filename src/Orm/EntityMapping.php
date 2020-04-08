74872218f70f5351f2e0487a43752f8eee530355
avolutions <acvogt89@gmail.com>
2020-04-08 22:05:42 +0200

    Add getFormularFields method to EntityMapping class

--- src/Orm/EntityMapping.php
+++ src/Orm/EntityMapping.php
@@ -60,5 +60,19 @@ class EntityMapping
 		}
 		
 		return [];
-	}
+    }
+    
+    /**
+	 * getFormularFields
+	 * 
+	 * Returns all fields where the form hidden attribute is not set or where it is false.
+     * 
+     * @return array An array with all Entity fields not hidden in formulars.
+	 */
+    public function getFormularFields()
+    {
+        return array_filter (get_object_vars($this), function($field) {
+            return isset($field['form']['hidden']) ? !$field['form']['hidden'] : true;
+        });
+    }
 }
