d19f02c72de1690bbbfd9b6d34e6d5b1d2a77e2e
avolutions <acvogt89@gmail.com>
2020-07-26 12:51:02 +0200

    Add tests bootstrap file

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * AVOLUTIONS
+ * 
+ * Just another open source PHP framework.
+ * 
+ * @copyright	Copyright (c) 2019 - 2020 AVOLUTIONS
+ * @license		MIT License (http://avolutions.org/license)
+ * @link		http://avolutions.org
+ */
+
+ require_once './bootstrap.php';
