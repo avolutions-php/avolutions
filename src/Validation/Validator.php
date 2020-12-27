8d473f87111a0a20a48b56b1a7d3ef9dcde6e206
avolutions <acvogt89@gmail.com>
2020-12-27 18:00:38 +0100

    Add validator interface and validator class

---
+++ src/Validation/Validator.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * AVOLUTIONS
+ * 
+ * Just another open source PHP framework.
+ * 
+ * @copyright	Copyright (c) 2019 - 2020 AVOLUTIONS
+ * @license		MIT License (http://avolutions.org/license)
+ * @link		http://avolutions.org
+ */
+
+namespace Avolutions\Validation;
+
+use Avolutions\Validation\ValidatorInterface;
+
+/**
+ * Validator
+ *
+ * TODO
+ * 
+ * @author	Alexander Vogt <alexander.vogt@avolutions.org>
+ * @since	0.6.0
+ */
+abstract class Validator implements ValidatorInterface
+{		
+    protected $options;
+
+    public function __construct($options) {
+        $this->options = $options;
+    }
+
+    public abstract function isValid($values);
+}
---
+++ src/Validation/ValidatorInterface.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * AVOLUTIONS
+ * 
+ * Just another open source PHP framework.
+ * 
+ * @copyright	Copyright (c) 2019 - 2020 AVOLUTIONS
+ * @license		MIT License (http://avolutions.org/license)
+ * @link		http://avolutions.org
+ */
+
+namespace Avolutions\Validation;
+
+/**
+ * Validator interface
+ *
+ * TODO
+ * 
+ * @author	Alexander Vogt <alexander.vogt@avolutions.org>
+ * @since	0.6.0
+ */
+interface ValidatorInterface
+{		
+	/**
+	 * isValid
+	 * 
+	 * TODO
+	 * 
+	 * @return bool TODO
+	 */
+	public function isValid($value);
+}
