55b8c7256b60a6db79c983c8135eb56916dbc134
AlxndrVgt <alexander.vogt@avolutions.org>
2020-12-28 12:06:43 +0100

    Add comments to validators

--- src/Validation/Validator/RequiredValidator.php
+++ src/Validation/Validator/RequiredValidator.php
@@ -1,11 +1,35 @@
 <?php
+/**
+ * AVOLUTIONS
+ * 
+ * Just another open source PHP framework.
+ * 
+ * @copyright	Copyright (c) 2019 - 2020 AVOLUTIONS
+ * @license		MIT License (http://avolutions.org/license)
+ * @link		http://avolutions.org
+ */
 
 namespace Avolutions\Validation\Validator;
 
 use Avolutions\Validation\Validator;
 
+/**
+ * RequiredValidator
+ *
+ * TODO
+ * 
+ * @author	Alexander Vogt <alexander.vogt@avolutions.org>
+ * @since	0.6.0
+ */
 class RequiredValidator extends Validator
 {
+    /**
+     * isValid
+     * 
+     * TODO
+     * 
+     * @return bool TODO
+     */
     public function isValid($value) {
         return !(
             is_null($value) || 
--- src/Validation/Validator/SizeValidator.php
+++ src/Validation/Validator/SizeValidator.php
@@ -1,11 +1,35 @@
 <?php
+/**
+ * AVOLUTIONS
+ * 
+ * Just another open source PHP framework.
+ * 
+ * @copyright	Copyright (c) 2019 - 2020 AVOLUTIONS
+ * @license		MIT License (http://avolutions.org/license)
+ * @link		http://avolutions.org
+ */
 
 namespace Avolutions\Validation\Validator;
 
 use Avolutions\Validation\Validator;
 
+/**
+ * SizeValidator
+ *
+ * TODO
+ * 
+ * @author	Alexander Vogt <alexander.vogt@avolutions.org>
+ * @since	0.6.0
+ */
 class SizeValidator extends Validator
 {
+    /**
+     * isValid
+     * 
+     * TODO
+     * 
+     * @return bool TODO
+     */
     public function isValid($value) {
         $size = $this->getSize($value);
 
