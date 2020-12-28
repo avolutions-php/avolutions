3bb1551ff580eb705f7a9bc70b2d54cc603ad41a
AlxndrVgt <alexander.vogt@avolutions.org>
2020-12-28 12:55:41 +0100

    Check for not false if using filter_var

--- src/Validation/Validator/FormatValidator.php
+++ src/Validation/Validator/FormatValidator.php
@@ -33,15 +33,15 @@ class FormatValidator extends Validator
     public function isValid($value) {
         switch($this->options) {
             case 'ip':
-                return filter_var($value, FILTER_VALIDATE_IP);
+                return filter_var($value, FILTER_VALIDATE_IP) !== false;
             case 'ip4':
-                return filter_var($value, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+                return filter_var($value, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) !== false;
             case 'ip6':
-                return filter_var($value, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6);
+                return filter_var($value, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) !== false;
             case 'mail':
-                return filter_var($value, FILTER_VALIDATE_EMAIL);
+                return filter_var($value, FILTER_VALIDATE_EMAIL) !== false;
             case 'url':
-                return filter_var($value, FILTER_VALIDATE_URL);
+                return filter_var($value, FILTER_VALIDATE_URL) !== false;
             case 'json':
                 json_decode($value);
                 return (json_last_error() == JSON_ERROR_NONE);
