7898132e6d86d71565f880c40a4e5f7eb46424ec
AlxndrVgt <alexander.vogt@avolutions.org>
2020-12-28 12:06:19 +0100

    Add FormatValidator

---
+++ src/Validation/Validator/FormatValidator.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * AVOLUTIONS
+ * 
+ * Just another open source PHP framework.
+ * 
+ * @copyright	Copyright (c) 2019 - 2020 AVOLUTIONS
+ * @license		MIT License (http://avolutions.org/license)
+ * @link		http://avolutions.org
+ */
+
+namespace Avolutions\Validation\Validator;
+
+use Avolutions\Validation\Validator;
+
+/**
+ * FormatValidator
+ *
+ * TODO
+ * 
+ * @author	Alexander Vogt <alexander.vogt@avolutions.org>
+ * @since	0.6.0
+ */
+class FormatValidator extends Validator
+{
+    /**
+     * isValid
+     * 
+     * TODO
+     * 
+     * @return bool TODO
+     */
+    public function isValid($value) {
+        switch($this->options) {
+            case 'ip':
+                return filter_var($value, FILTER_VALIDATE_IP);
+            case 'ip4':
+                return filter_var($value, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+            case 'ip6':
+                return filter_var($value, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6);
+            case 'mail':
+                return filter_var($value, FILTER_VALIDATE_EMAIL);
+            case 'url':
+                return filter_var($value, FILTER_VALIDATE_URL);
+            case 'json':
+                json_decode($value);
+                return (json_last_error() == JSON_ERROR_NONE);
+            default:
+                return false;
+        }
+    }
+}
