95a4ff6dd78904e01866ffc6b1cc3886c1213845
Alexander Vogt <alexander.vogt@avolutions.org>
2021-01-24 20:53:06 +0100

    Add message to validators

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -11,7 +11,8 @@
 
 namespace Avolutions\Validation;
 
-use Avolutions\Validation\ValidatorInterface;
+use Exception;
+use ReflectionClass;
 
 /**
  * Validator
@@ -31,7 +32,7 @@ abstract class Validator implements ValidatorInterface
     /**
 	 * TODO
 	 */
-    protected $property = [];
+    protected $property = null;
 
     /**
 	 * TODO
@@ -39,9 +40,17 @@ abstract class Validator implements ValidatorInterface
     protected $Entity = null;
 
     /**
+     * TODO
+     */
+    protected $message = null;
+
+    /**
      * __construct
      *
      * TODO
+     * @param array $options
+     * @param null $property
+     * @param null $Entity
      */
     public function __construct($options = [], $property = null, $Entity = null) {
         $this->setOptions($options, $property, $Entity);
@@ -51,6 +60,8 @@ abstract class Validator implements ValidatorInterface
      * getSize
      *
      * TODO
+     * @param $value
+     * @return int|string
      */
     protected function getSize($value) {
         if (is_numeric($value)) {
@@ -66,17 +77,67 @@ abstract class Validator implements ValidatorInterface
      * isValid
      *
      * TODO
+     * @param $value
      */
-    abstract public function isValid($values);
+    abstract public function isValid($value);
 
     /**
      * setOptions
      *
      * TODO
+     * @param array $options
+     * @param null $property
+     * @param null $Entity
      */
     public function setOptions($options = [], $property = null, $Entity = null) {
         $this->options = $options;
+        if (isset($options['message'])) {
+            $this->message = $options['message'];
+        }
         $this->property = $property;
         $this->Entity = $Entity;
+    }
+
+    /**
+     * getValidatorName
+     *
+     * Returns the name of the validator.
+     *
+     * @return string The name of the validator.
+     */
+    protected function getValidatorName()
+    {
+        $validatorName = (new ReflectionClass($this))->getShortName();
+        $validatorName = strtolower(str_replace(VALIDATOR, '', $validatorName));
+
+        return $validatorName;
+    }
+
+    /**
+     * TODO
+     */
+    public function getMessage() {
+        if (!is_null($this->message)) {
+            return $this->message;
+        } else {
+            $validatorKey = 'validation/'.$this->getValidatorName();
+
+            try {
+                if (!is_null($this->property)) {
+                    if (!is_null($this->Entity)) {
+                        return translate($validatorKey.'/'.$this->Entity->getEntityName().'/'.$this->property);
+                    }
+
+                    return translate($validatorKey.'/'.$this->property);
+                }
+
+                // TODO specify message for Entity and property?
+                // TODO use name of validator
+                return translate($validatorKey);
+            } catch (Exception $ex) {
+                // TODO use label of property?
+                return $this->property.' is not valid';
+            }
+        }
     }
 }
