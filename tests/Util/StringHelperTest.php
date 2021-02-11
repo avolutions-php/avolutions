707d5ab50c625909cd2352ea63c38d0d663acf46
Alexander Vogt <alexander.vogt@avolutions.org>
2021-02-11 20:57:32 +0100

    Add StringHelper tests

---
+++ tests/Util/StringHelperTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * AVOLUTIONS
+ *
+ * Just another open source PHP framework.
+ *
+ * @copyright	Copyright (c) 2019 - 2021 AVOLUTIONS
+ * @license		MIT License (http://avolutions.org/license)
+ * @link		http://avolutions.org
+ */
+
+use Avolutions\Util\StringHelper;
+use PHPUnit\Framework\TestCase;
+
+class StringHelperTest extends TestCase
+{
+    public function testInterpolate()
+    {
+        $stringWithNumericPlaceholders = 'Hey, my name is {0}. I\'m {1} years old.';
+        $stringWithNamedPlaceholders = 'Hey, my name is {name}. I\'m {age} years old.';
+
+        $this->assertEquals('Hey, my name is Alex. I\'m 42 years old.', StringHelper::interpolate($stringWithNumericPlaceholders, ['Alex', 42]));
+        $this->assertEquals('Hey, my name is Alex. I\'m 42 years old.', StringHelper::interpolate($stringWithNamedPlaceholders, ['age' => 42, 'name' => 'Alex']));
+    }
+}
