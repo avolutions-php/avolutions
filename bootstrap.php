a0a985825123d8350b78c1333382712df12695dc
Alexander Vogt <alexander.vogt@avolutions.org>
2021-06-08 22:16:53 +0200

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -16,24 +16,24 @@ print 'test';
  * Define paths
  */
 define(__NAMESPACE__.'\\BASE_PATH', realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR);
-define('CONFIG_PATH', BASE_PATH.'config'.DIRECTORY_SEPARATOR);
+define(__NAMESPACE__.'\\CONFIG_PATH', BASE_PATH.'config'.DIRECTORY_SEPARATOR);
 
-define('APPLICATION_PATH', BASE_PATH.'application'.DIRECTORY_SEPARATOR);
-define('APP_CONFIG_PATH', APPLICATION_PATH.'Config'.DIRECTORY_SEPARATOR);
-define('APP_DATABASE_PATH', APPLICATION_PATH.'Database'.DIRECTORY_SEPARATOR);
-define('APP_MAPPING_PATH', APPLICATION_PATH.'Mapping'.DIRECTORY_SEPARATOR);
-define('APP_VIEW_PATH', APPLICATION_PATH.'View'.DIRECTORY_SEPARATOR);
-define('APP_TRANSLATION_PATH', APPLICATION_PATH.'Translation'.DIRECTORY_SEPARATOR);
+define(__NAMESPACE__.'\\APPLICATION_PATH', BASE_PATH.'application'.DIRECTORY_SEPARATOR);
+define(__NAMESPACE__.'\\APP_CONFIG_PATH', APPLICATION_PATH.'Config'.DIRECTORY_SEPARATOR);
+define(__NAMESPACE__.'\\APP_DATABASE_PATH', APPLICATION_PATH.'Database'.DIRECTORY_SEPARATOR);
+define(__NAMESPACE__.'\\APP_MAPPING_PATH', APPLICATION_PATH.'Mapping'.DIRECTORY_SEPARATOR);
+define(__NAMESPACE__.'\\APP_VIEW_PATH', APPLICATION_PATH.'View'.DIRECTORY_SEPARATOR);
+define(__NAMESPACE__.'\\APP_TRANSLATION_PATH', APPLICATION_PATH.'Translation'.DIRECTORY_SEPARATOR);
 
 /**
  * Define namespace
  */
-define('VALIDATOR_NAMESPACE', 'Avolutions\\Validation\\');
-define('APP_CONTROLLER_NAMESPACE', 'Application\\Controller\\');
-define('APP_DATABASE_NAMESPACE', 'Application\\Database\\');
-define('APP_LISTENER_NAMESPACE', 'Application\\Listener\\');
-define('APP_MODEL_NAMESPACE', 'Application\\Model\\');
-define('APP_VALIDATOR_NAMESPACE', 'Application\\Validation\\');
+define(__NAMESPACE__.'\\VALIDATOR_NAMESPACE', 'Avolutions\\Validation\\');
+define(__NAMESPACE__.'\\APP_CONTROLLER_NAMESPACE', 'Application\\Controller\\');
+define(__NAMESPACE__.'\\APP_DATABASE_NAMESPACE', 'Application\\Database\\');
+define(__NAMESPACE__.'\\APP_LISTENER_NAMESPACE', 'Application\\Listener\\');
+define(__NAMESPACE__.'\\APP_MODEL_NAMESPACE', 'Application\\Model\\');
+define(__NAMESPACE__.'\\APP_VALIDATOR_NAMESPACE', 'Application\\Validation\\');
 
 
 get_defined_constants();
