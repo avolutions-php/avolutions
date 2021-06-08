1c6deac295d36947b73b7d3e8efda50ff0237adc
Alexander Vogt <alexander.vogt@avolutions.org>
2021-06-08 20:20:24 +0200

    Add default configs

---
+++ config/database.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * AVOLUTIONS
+ *
+ * Just another open source PHP framework.
+ *
+ * @copyright   Copyright (c) 2019 - 2021 AVOLUTIONS
+ * @license     MIT License (http://avolutions.org/license)
+ * @link        http://avolutions.org
+ */
+
+return [
+	'host' => getenv('DB_HOST') ?: '127.0.0.1',
+	'database' => getenv('DB_DATABASE') ?: 'avolutions',
+	'port' => getenv('DB_PORT') ?: '3306',
+	'user' => getenv('DB_USER') ?: 'avolutions',
+	'password' => getenv('DB_PASSWORD') ?: 'avolutions',
+	'charset' => 'utf8',
+	'migrateOnAppStart' => false
+];
---
+++ config/logger.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * AVOLUTIONS
+ *
+ * Just another open source PHP framework.
+ *
+ * @copyright   Copyright (c) 2019 - 2021 AVOLUTIONS
+ * @license     MIT License (http://avolutions.org/license)
+ * @link        http://avolutions.org
+ */
+
+use Avolutions\Logging\LogLevel;
+
+return [
+	'loglevel' => LogLevel::DEBUG,
+	'logfile' => 'logfile.log',
+	'logpath' => 'log/',
+	'datetimeFormat' => 'Y-m-d H:i:s.v'
+];
