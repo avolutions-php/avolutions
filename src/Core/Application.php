1d92e401fb731c379004e74eb7ff4c0704cbaf7c
Alexander Vogt <alexander.vogt@avolutions.org>
2021-08-02 13:22:48 +0200

    Add comments and formatting to Application class

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -11,74 +11,151 @@
 
 namespace Avolutions\Core;
 
+use Avolutions\Util\JsonHelper;
+
 /**
  * Application class
  *
- * TODO
+ * This class is responsible for path and namespace management of the Application.
  *
  * @author	Alexander Vogt <alexander.vogt@avolutions.org>
  * @since	0.8.0
  */
 class Application extends AbstractSingleton
 {
-    // TODO idea: add bool parameter to getter to add DIRECTORY_SEPERATOR (default) or not to end of path
-    // TODO change to protected to allow users to redefined by extending/override Application class with DI
-    // TODO if paths are set by developer then return in getter methods
+    /**
+     * The base path of your application.
+     *
+     * @var string
+     */
     private static string $basePath;
-    private string $appPath;
-    private string $commandPath;
-    private string $configPath;
-    private string $databasePath; // TODO change to migration?
-    private string $mappingPath;
-    private string $translationPath;
-    private string $viewPath;
-
-    // TODO add variables for namespaces to provide override
-    private static string $appNamespace;
-    protected static string $commandNamespace;
-    protected static string $controllerNamespace;
-    protected static string $databaseNamespace;
-    protected static string $listenerNamespace;
-    protected static string $modelNamespace;
-    protected static string $validatorNamespace;
-
 
     /**
-     * @return string
+     * The application namespace.
+     *
+     * @var string
      */
-    public static function getCommandNamespace(): string
+    private static string $appNamespace;
+
+    /**
+     * initialize
+     *
+     * Initializes the application.
+     *
+     * @param string $basePath The base path of your application.
+     */
+    public function initialize(string $basePath = '')
     {
-        return self::$appNamespace.'Command\\';
+        self::$basePath = rtrim($basePath, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
+        self::$appNamespace = self::getAppNamespace();
     }
 
-    public static function getControllerNamespace(): string
+    /**
+     * getBasePath
+     *
+     * Returns the base path.
+     *
+     * @return string The base path.
+     */
+    public function getBasePath(): string
     {
-        return self::$appNamespace.'Controller\\';
+        return self::$basePath;
     }
 
-    public static function getDatabaseNamespace(): string
+    /**
+     * getAppPath
+     *
+     * Returns the application path.
+     *
+     * @return string The application path.
+     */
+    public static function getAppPath(): string
     {
-        return self::$appNamespace.'Database\\';
+        return self::$basePath.'application'.DIRECTORY_SEPARATOR;
     }
 
-    public static function getListenerNamespace(): string
+    /**
+     * getCommandPath
+     *
+     * Returns the path where Commands are stored.
+     *
+     * @return string The command path.
+     */
+    public static function getCommandPath(): string
     {
-        return self::$appNamespace.'Listener\\';
+        return self::getAppPath().'Command'.DIRECTORY_SEPARATOR;
     }
 
-    public static function getModelNamespace(): string
+    /**
+     * getConfigPath
+     *
+     * Returns the path where config files are stored.
+     *
+     * @return string The config path.
+     */
+    public static function getConfigPath(): string
     {
-        return self::$appNamespace.'Model\\';
+        return self::getAppPath().'Config'.DIRECTORY_SEPARATOR;
     }
 
-    public static function getValidatorNamespace(): string
+    /**
+     * getDatabasePath
+     *
+     * Returns the path where database migrations are stored.
+     *
+     * @return string The database path.
+     */
+    public static function getDatabasePath(): string
     {
-        return self::$appNamespace.'Validator\\';
+        return self::getAppPath().'Database'.DIRECTORY_SEPARATOR;
     }
 
+    /**
+     * getMappingPath
+     *
+     * Returns the path where mapping files are stored.
+     *
+     * @return string The mapping path.
+     */
+    public static function getMappingPath(): string
+    {
+        return self::getAppPath().'Mapping'.DIRECTORY_SEPARATOR;
+    }
+
+    /**
+     * getTranslationPath
+     *
+     * Returns the path where translation files are stored.
+     *
+     * @return string The translation path.
+     */
+    public static function getTranslationPath(): string
+    {
+        return self::getAppPath().'Translation'.DIRECTORY_SEPARATOR;
+    }
+
+    /**
+     * getViewPath
+     *
+     * Returns the path where Views are stored.
+     *
+     * @return string The view path.
+     */
+    public static function getViewPath(): string
+    {
+        return self::getAppPath().'View'.DIRECTORY_SEPARATOR;
+    }
+
+    /**
+     * getAppNamespace
+     *
+     * Returns the namespace for your application by reading it from composer.json.
+     *
+     * @return string The application namespace.
+     */
     private static function getAppNamespace(): string
     {
-        $composer = json_decode(file_get_contents(self::$basePath.'composer.json'), true);
+        $composer = JsonHelper::decode(self::$basePath.'composer.json', true);
 
         foreach ($composer["autoload"]["psr-4"] as $namespace => $path) {
             if (realpath(self::$basePath.$path) === realpath(self::getAppPath())) {
@@ -88,79 +165,74 @@ class Application extends AbstractSingleton
     }
 
     /**
-     * Application constructor.
+     * getCommandNamespace
      *
-     * @param string $basePath
+     * Returns the namespace for Command classes.
+     *
+     * @return string The command namespace.
      */
-    public function initialize(string $basePath = '')
+    public static function getCommandNamespace(): string
     {
-        // TODO check passed basePath and add DIRECTORY_SEPERATOR to end if not there
-        self::$basePath = $basePath.DIRECTORY_SEPARATOR;
-        self::$appNamespace = self::getAppNamespace();
-    }
-
-
-    /**
-     * @return string
-     */
-    public function getBasePath(): string
-    {
-        return self::$basePath;
+        return self::$appNamespace.'Command\\';
     }
 
     /**
-     * @return string
+     * getControllerNamespace
+     *
+     * Returns the namespace for Controller classes.
+     *
+     * @return string The controller namespace.
      */
-    public static function getAppPath(): string
+    public static function getControllerNamespace(): string
     {
-        return self::$basePath.'application'.DIRECTORY_SEPARATOR;
+        return self::$appNamespace.'Controller\\';
     }
 
     /**
-     * @return string
+     * getDatabaseNamespace
+     *
+     * Returns the namespace for Database Migration classes.
+     *
+     * @return string The database namespace.
      */
-    public static function getCommandPath(): string
+    public static function getDatabaseNamespace(): string
     {
-        return self::getAppPath().'Command'.DIRECTORY_SEPARATOR;
+        return self::$appNamespace.'Database\\';
     }
 
     /**
-     * @return string
+     * getListenerNamespace
+     *
+     * Returns the namespace for Listener classes.
+     *
+     * @return string The listener namespace.
      */
-    public static function getConfigPath(): string
+    public static function getListenerNamespace(): string
     {
-        return self::getAppPath().'Config'.DIRECTORY_SEPARATOR;
+        return self::$appNamespace.'Listener\\';
     }
 
     /**
-     * @return string
+     * getModelNamespace
+     *
+     * Returns the namespace for Model classes.
+     *
+     * @return string The model namespace.
      */
-    public static function getDatabasePath(): string
+    public static function getModelNamespace(): string
     {
-        return self::getAppPath().'Database'.DIRECTORY_SEPARATOR;
+        return self::$appNamespace.'Model\\';
     }
 
     /**
-     * @return string
+     * getValidatorNamespace
+     *
+     * Returns the namespace for Validator classes.
+     *
+     * @return string The validator namespace.
      */
-    public static function getMappingPath(): string
+    public static function getValidatorNamespace(): string
     {
-        return self::getAppPath().'Mapping'.DIRECTORY_SEPARATOR;
-    }
-
-    /**
-     * @return string
-     */
-    public static function getTranslationPath(): string
-    {
-        return self::getAppPath().'Translation'.DIRECTORY_SEPARATOR;
-    }
-
-    /**
-     * @return string
-     */
-    public static function getViewPath(): string
-    {
-        return self::getAppPath().'View'.DIRECTORY_SEPARATOR;
+        return self::$appNamespace.'Validator\\';
     }
 }
