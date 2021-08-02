b3bea133ac975890c45e3ca6fe6caee40ae6ffdd
Alexander Vogt <alexander.vogt@avolutions.org>
2021-08-02 13:22:03 +0200

    Revert changes from Reponse class

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -11,6 +11,8 @@
 
 namespace Avolutions\Http;
 
+use Avolutions\View\View;
+
 /**
  * Response class
  *
@@ -21,64 +23,12 @@ namespace Avolutions\Http;
  */
 class Response
 {
-	/**
-     * The header keys and values of the response.
-     *
-	 * @var array $headers
-	 */
-	private array $headers = [];
-
     /**
      * The content of the response.
      *
      * @var string $body
      */
-    private string $body;
-
-    /**
-     * __construct
-     *
-     * TODO
-     *
-     * @param string $body
-     * @param array $headers
-     */
-    public function __construct(string $body = '', array $headers = [])
-    {
-        $this->setBody($body);
-        $this->setHeaders($headers);
-    }
-
-
-    /**
-	 * send
-	 *
-	 * Displays the content of the Response.
-	 */
-    public function send()
-    {
-        $this->sendHeaders();
-        $this->sendBody();
-	}
-
-	/**
-     * TODO
-     */
-    public function sendBody()
-    {
-        print $this->body;
-    }
-
-    /**
-     * TODO
-     */
-	public function sendHeaders()
-    {
-        foreach ($this->headers as $name => $value) {
-            // TODO replace and status code?
-            header($name.':'.$value);
-        }
-    }
+    public string $body;
 
     /**
      * setBody
@@ -87,35 +37,18 @@ class Response
      *
      * @param string $value The value for the body
      */
-    public function setBody(?string $value)
+    public function setBody(string $value)
     {
-        $this->body = $value ?? '';
+        $this->body = $value;
     }
 
     /**
-     * setHeaders
+     * send
      *
-     * TODO
-     *
-     * @param string $name TODO
-     * @param mixed $value TODO
+     * Displays the content of the Response.
      */
-    public function setHeader(string $name, mixed $value): void
+    public function send()
     {
-        $this->headers[$name] = $value;
-    }
-
-    /**
-     * setHeaders
-     *
-     * TODO
-     *
-     * @param array $headers TODO
-     */
-    public function setHeaders(array $headers = []): void
-    {
-        foreach ($headers as $name => $value) {
-            $this->setHeader($name, $value);
-        }
+        print $this->body;
     }
 }
