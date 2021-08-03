d0ab8e9054574b0aa68082ec909fe02c759f4e78
Alexander Vogt <alexander.vogt@avolutions.org>
2021-08-03 22:18:48 +0200

    Add name as property to Command

--- src/Command/Command.php
+++ src/Command/Command.php
@@ -2,25 +2,29 @@
 
 namespace Avolutions\Command;
 
+use Avolutions\Console\Console;
 use ReflectionClass;
 
 abstract class Command
 {
+    protected static string $name = '';
     protected static string $description = '';
     public CommandDefinition $CommandDefinition;
+    protected Console $Console;
 
     private array $arguments = [];
     private array $options = [];
 
-    public function __construct()
+    public function __construct(Console $Console)
     {
+        $this->Console = $Console;
         $this->CommandDefinition = new CommandDefinition();
         $this->initialize();
     }
 
     public static function getName(): string
     {
-        return str_replace('Command', '', (new ReflectionClass(get_called_class()))->getShortName());
+        return static::$name === '' ? str_replace('Command', '', (new ReflectionClass(get_called_class()))->getShortName()) : static::$name;
     }
 
     public static function getDescription(): string
@@ -48,6 +52,6 @@ abstract class Command
 
     // TODO addDefinition method to pass a ArgumentDefinition type?
 
-    abstract public function execute();
+    abstract public function execute(): int;
     abstract public function initialize();
 }
