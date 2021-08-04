03b7fbcfee490f4eca692aeebc2a046bf59397c2
Alexander Vogt <alexander.vogt@avolutions.org>
2021-08-04 14:30:42 +0200

    Add optional to Argument

--- src/Command/Argument.php
+++ src/Command/Argument.php
@@ -1,17 +1,47 @@
 <?php
-
+/**
+ * TODO
+ */
 
 namespace Avolutions\Command;
 
-
+/**
+ * TODO
+ */
 class Argument
 {
+    /**
+     * TODO
+     *
+     * @var string
+     */
     public string $name;
+
+    /**
+     * TODO
+     *
+     * @var string|mixed
+     */
     public string $help;
 
-    public function __construct($name, $help = '')
+    /**
+     * TODO
+     *
+     * @var bool|mixed
+     */
+    public bool $optional;
+
+    /**
+     * TODO
+     *
+     * @param string $name TODO
+     * @param string $help TODO
+     * @param bool $optional TODO
+     */
+    public function __construct(string $name, string $help = '', bool $optional = false)
     {
         $this->name = $name;
         $this->help = $help;
+        $this->optional = $optional;
     }
 }
