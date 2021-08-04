c2af1bda4ad22d7dc99ddd42bc3cd0e64cb87fc8
Alexander Vogt <alexander.vogt@avolutions.org>
2021-08-04 14:30:58 +0200

    Set default value of Option to false

--- src/Command/Option.php
+++ src/Command/Option.php
@@ -1,17 +1,52 @@
 <?php
-
+/**
+ * TODO
+ */
 
 namespace Avolutions\Command;
 
-
+/**
+ * TODO
+ */
 class Option
 {
+    /**
+     * TODO
+     *
+     * @var string
+     */
     public string $name;
+
+    /**
+     * TODO
+     *
+     * @var string|mixed
+     */
     public string $short;
+
+    /**
+     * TODO
+     *
+     * @var string|mixed
+     */
     public string $help;
+
+    /**
+     * TODO
+     *
+     * @var mixed|null
+     */
     public mixed $default;
 
-    public function __construct($name, $short = '', $help = '', $default = null)
+    /**
+     * TODO
+     *
+     * @param string $name TODO
+     * @param string $short TODO
+     * @param string $help TODO
+     * @param bool $default TODO
+     */
+    public function __construct(string $name, string $short = '', string $help = '', bool $default = false)
     {
         $this->name = $name;
         $this->short = $short;
