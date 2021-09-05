dd7f1a8b1375167bbe841a33cda94cf2d14e8689
Alexander Vogt <alexander.vogt@avolutions.org>
2021-09-05 15:28:40 +0200

    Work an template parsing

--- src/Template/TemplateParser.php
+++ src/Template/TemplateParser.php
@@ -3,7 +3,6 @@
 
 namespace Avolutions\Template;
 
-
 class TemplateParser
 {
     private string $startTag = '{{';
@@ -66,11 +65,7 @@ class TemplateParser
             return Token::END;
         }
 
-        if (str_starts_with($match, '$')) {
-            return Token::VARIABLE;
-        }
-
-        return Token::UNKOWN;
+        return Token::VARIABLE;
     }
 
     public function parse(array $tokens)
@@ -96,21 +91,32 @@ class TemplateParser
 
     private function parseIf($Token): string
     {
-        if (preg_match('@if \$?([a-zA-Z0-9_-]*) (==|!=) \$?([a-zA-Z0-9]*)@', $Token->value, $matches)) {
-            // TODO parse matches[1]: wenn variable, dann durch $data ersetzen, wenn string dann anführungszeichen, wenn bool oder zahl dann nichts
-            // TODO parse matches[2]: wenn equal/is/=== etc durch === etc ersetzen
-            // TODO parse matches[3]: wenn variable, dann durch $data ersetzen, wenn string dann anführungszeichen, wenn bool oder zahl dann nichts
+        // TODO multiple regex for e.g. {{ if true }} or {{ if not true }}
+        if (preg_match('@if (\$?[a-zA-Z0-9_-]*) (==|!=) (\$?[a-zA-Z0-9]*)@', $Token->value, $matches)) {
+            $ifCondition = 'if (';
+            $ifCondition .= $this->todoParseIfTerm($matches[1]);
+            $ifCondition .= ' ' . $this->todoParseIfOperator($matches[2]) . ' ';
+            $ifCondition .= $this->todoParseIfTerm($matches[3]);
+            $ifCondition .= ') {'.PHP_EOL;
 
-            return str_replace('if', 'if (', $Token->value) . ') {'.PHP_EOL;
+            return $ifCondition;
         } else {
-            // throw Expection
+            // throw Exception
         }
-
-        return "";
     }
     private function parseFor($Token): string
     {
-        return "";
+        if (preg_match('@for ([a-zA-Z0-9_-]*) (in) (\$[a-zA-Z0-9_-]*)@', $Token->value, $matches)) {
+            $forLoop = 'foreach (';
+            $forLoop .= $this->todoVariable($matches[3]);
+            $forLoop .= ' as ';
+            $forLoop .= $this->todoVariable($matches[1], 'local');
+            $forLoop .= ') {'.PHP_EOL;
+
+            return $forLoop;
+        } else {
+            // throw Exception
+        }
     }
 
     private function parseElse($Token): string
@@ -118,7 +124,7 @@ class TemplateParser
         if (preg_match('@else@', $Token->value, $matches)) {
             return '} else {'.PHP_EOL;
         } else {
-            // throw exception
+            // throw Exception
         }
     }
 
@@ -127,27 +133,73 @@ class TemplateParser
         if (preg_match('@(/|end)(if|for)@', $Token->value, $matches)) {
             return '}'.PHP_EOL;
         } else {
-            // throw exception
+            // throw Exception
         }
     }
 
     private function parseVariable($Token): string
     {
-        if (preg_match('@\$([a-zA-Z0-9_-]*)@', $Token->value, $matches)) {
-            return 'print ' . str_replace($matches[1], $this->todoVariable($matches[1]), $Token->value).PHP_EOL;
+        // TODO one regex to match global and local ($ = optional)
+        if (preg_match('@(\$[a-zA-Z0-9_-]*)(.[a-zA-Z0-9_-]*)*@', $Token->value, $matches)) {
+            // global
+            return 'print ' . str_replace($matches[0], $this->todoVariable($matches[0]), $Token->value) . ';' . PHP_EOL;
+        } else if (preg_match('@([a-zA-Z0-9_-]*.[a-zA-Z0-9_-])*@', $Token->value, $matches)) {
+            // local
+            return 'print ' . str_replace($matches[0], $this->todoVariable($matches[0], 'local'), $Token->value) . ';' . PHP_EOL;
         } else {
-            // throw Expection
+            // throw Exception
         }
     }
 
-    private function todoVariable($variable): string
+    private function todoVariable($variable, $scope = 'global'): string
     {
-        return 'data["' . $variable . '"];';
+        $parsedVariable = '';
+        $explodedVariable = explode('.', $variable);
+
+        if ($scope == 'global') {
+            $parsedVariable = '$data';
+        }
+
+        if ($scope == 'local') {
+            $parsedVariable = '$' . trim($explodedVariable[0]);
+            unset($explodedVariable[0]);
+        }
+
+        foreach ($explodedVariable as $name) {
+            $parsedVariable .= '["' . trim(ltrim($name, '$')) . '"]';
+        }
+
+        return $parsedVariable;
     }
 
     private function todoPrint(): string
     {
         // TODO add print, semicolon and new line
         return "";
+    }
+
+    /**
+     * @param $haystack
+     * @param mixed $ifCondition
+     * @return array|mixed|string|string[]
+     */
+    private function todoParseIfTerm($ifTerm): mixed
+    {
+        // TODO wenn variable, dann durch $data ersetzen, wenn string dann anführungszeichen, wenn bool oder zahl dann nichts
+        if (str_starts_with($ifTerm, '$')) {
+            return $this->todoVariable($ifTerm);
+        }
+
+        // TODO check bool
+        if (!is_numeric($ifTerm)) {
+            return str_replace($ifTerm, '"' . $ifTerm . '"', $ifTerm);
+        }
+
+        return $ifTerm;
+    }
+
+    private function todoParseIfOperator($ifOperator)
+    {
+        return $ifOperator;
     }
 }
