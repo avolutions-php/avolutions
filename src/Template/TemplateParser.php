9d38964a5ded571f43b51c379dba32a7f9832e1f
Alexander Vogt <alexander.vogt@avolutions.org>
2021-09-07 15:11:50 +0200

    Add and use variable regex

--- src/Template/TemplateParser.php
+++ src/Template/TemplateParser.php
@@ -8,6 +8,8 @@ class TemplateParser
     private string $startTag = '{{';
     private string $endTag = '}}';
 
+    private string $validVariableCharacters = '[a-zA-Z0-9_-]';
+
     private int $position = 0;
     private array $tokens = [];
 
@@ -32,10 +34,10 @@ class TemplateParser
                 $this->position = $this->position + $diff;
             }
 
-            $matchWithDelimiter = $matchesWithoutDelimiter[$index][0];
+            $matchWithoutDelimiter = $matchesWithoutDelimiter[$index][0];
             $this->tokens[] = new Token(
-                $this->getTokenType($matchWithDelimiter),
-                $matchWithDelimiter
+                $this->getTokenType($matchWithoutDelimiter),
+                trim($matchWithoutDelimiter)
             );
             $this->position = $offset + $length;
         }
@@ -92,7 +94,7 @@ class TemplateParser
     private function parseIf($Token): string
     {
         // TODO multiple regex for e.g. {{ if true }} or {{ if not true }}
-        if (preg_match('@if (\$?[a-zA-Z0-9_-]*) (==|!=) (\$?[a-zA-Z0-9]*)@', $Token->value, $matches)) {
+        if (preg_match('@if (.*) (==|!=) (.*)@', $Token->value, $matches)) {
             $ifCondition = 'if (';
             $ifCondition .= $this->todoParseIfTerm($matches[1]);
             $ifCondition .= ' ' . $this->todoParseIfOperator($matches[2]) . ' ';
@@ -106,9 +108,12 @@ class TemplateParser
     }
     private function parseFor($Token): string
     {
-        if (preg_match('@for ([a-zA-Z0-9_-]*) (in) (\$[a-zA-Z0-9_-]*)@', $Token->value, $matches)) {
-            $forLoop = 'foreach (';
-            $forLoop .= $this->todoVariable($matches[3]);
+        if (preg_match('@for\s(' . $this->validVariableCharacters . '*)\sin\s(' . $this->getVariableRegex(false) . ')@x', $Token->value, $matches)) {
+            $variable = $this->todoVariable($matches[2]);
+
+            $forLoop = 'if (isset(' . $variable . ')) { ';
+            $forLoop .= 'foreach (';
+            $forLoop .= $variable;
             $forLoop .= ' as ';
             $forLoop .= $this->todoVariable($matches[1], 'local');
             $forLoop .= ') {'.PHP_EOL;
@@ -130,8 +135,14 @@ class TemplateParser
 
     private function parseEnd($Token): string
     {
-        if (preg_match('@(/|end)(if|for)@', $Token->value, $matches)) {
-            return '}'.PHP_EOL;
+        if (preg_match('@(?:/|end)(if|for)@', $Token->value, $matches)) {
+            $end = '}'.PHP_EOL;
+            if ($matches[1] == 'for') {
+                // close if (isset())) {
+                $end .= '}'.PHP_EOL;
+            }
+
+            return $end;
         } else {
             // throw Exception
         }
@@ -139,28 +150,24 @@ class TemplateParser
 
     private function parseVariable($Token): string
     {
-        // TODO one regex to match global and local ($ = optional)
-        if (preg_match('@(\$[a-zA-Z0-9_-]*)(.[a-zA-Z0-9_-]*)*@', $Token->value, $matches)) {
-            // global
+        if (preg_match('@' . $this->getVariableRegex() . '@x', $Token->value, $matches)) {
             return 'print ' . str_replace($matches[0], $this->todoVariable($matches[0]), $Token->value) . ';' . PHP_EOL;
-        } else if (preg_match('@([a-zA-Z0-9_-]*.[a-zA-Z0-9_-])*@', $Token->value, $matches)) {
-            // local
-            return 'print ' . str_replace($matches[0], $this->todoVariable($matches[0], 'local'), $Token->value) . ';' . PHP_EOL;
         } else {
             // throw Exception
         }
     }
 
-    private function todoVariable($variable, $scope = 'global'): string
+    private function todoVariable($variable): string
     {
+        $variable = trim($variable);
         $parsedVariable = '';
         $explodedVariable = explode('.', $variable);
 
-        if ($scope == 'global') {
+        if (str_starts_with($variable, '$')) {
+            // global
             $parsedVariable = '$data';
-        }
-
-        if ($scope == 'local') {
+        } else {
+            // local
             $parsedVariable = '$' . trim($explodedVariable[0]);
             unset($explodedVariable[0]);
         }
@@ -186,12 +193,11 @@ class TemplateParser
     private function todoParseIfTerm($ifTerm): mixed
     {
         // TODO wenn variable, dann durch $data ersetzen, wenn string dann anführungszeichen, wenn bool oder zahl dann nichts
-        if (str_starts_with($ifTerm, '$')) {
+        if (preg_match('@' . $this->getVariableRegex() . '@x', $ifTerm, $matches)) {
             return $this->todoVariable($ifTerm);
         }
 
-        // TODO check bool
-        if (!is_numeric($ifTerm)) {
+        if (!is_numeric($ifTerm) && !in_array($ifTerm, ['false', 'true'])) {
             return str_replace($ifTerm, '"' . $ifTerm . '"', $ifTerm);
         }
 
@@ -201,5 +207,38 @@ class TemplateParser
     private function todoParseIfOperator($ifOperator)
     {
         return $ifOperator;
+    }
+
+    private function getVariableRegex($stringStartEnd = true)
+    {
+        // Variables can either have a global or local scope, e.g.:
+        //      Global: {{ $foo }} or {{ $foo.bar }}
+        //      Local: {{ foo.bar }
+        // Global variable consists of two parts:
+        //      1. Starts with exact one "$" followed by 1 to n allowed variable characters.
+        //         This part is required exact once. E.g. "$foo"
+        //      2. Starts with exact one "." followed by 1 to n allowed variable characters.
+        //         This part can occur 0 to n times. E.g. ".bar"
+        // Local variable consists of two parts:
+        //      1. Starts with 1 to n allowed variable characters followed by exact on ".".
+        //         This part can occur 1 to n times. E.g.: "foo."
+        //      2. Starts with 1 to n allowed variable characters.
+        //         This part is required exact once. E.g. "bar"
+        $globalVariableRegex = '
+            (?:\${1}' . $this->validVariableCharacters . '{1,}){1}
+            (?:\.{1}' . $this->validVariableCharacters . '{1,})*
+        ';
+
+        $localVariableRegex = '
+            (?:' . $this->validVariableCharacters . '{1,}\.{1}){1,}
+            (?:' . $this->validVariableCharacters . '{1,}){1}
+        ';
+
+        if ($stringStartEnd) {
+            $globalVariableRegex = '^' . $globalVariableRegex . '$';
+            $localVariableRegex = '^' . $localVariableRegex . '$';
+        }
+
+        return $globalVariableRegex. '|' . $localVariableRegex;
     }
 }
