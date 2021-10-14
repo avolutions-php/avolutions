42b79025ec385f2f5a680328ebd1b3d25ea803af
Alexander Vogt <alexander.vogt@avolutions.org>
2021-10-15 00:09:39 +0200

    Add switch case for end parsing

--- src/Template/TemplateParser.php
+++ src/Template/TemplateParser.php
@@ -237,22 +237,31 @@ class TemplateParser
     private function parseEnd(Token $Token): string
     {
         // TODO own methods
-        if (preg_match('@(?:/|end)(if|for)@', $Token->value, $matches)) {
-            if ($matches[1] == 'for') {
-                $end = '$loop["index"]++;' . PHP_EOL;
-                $end .= '$loop["first"] = false;' . PHP_EOL;
-                $end .= '}'.PHP_EOL;
-                $end .= 'if (isset($loop["parent"])) {' . PHP_EOL;
-                $end .= "\t" . '$loop = $loop["parent"];' . PHP_EOL;
-                $end .= '}' . PHP_EOL;
-                $end .= '}' . PHP_EOL;
-            } else {
-                $end = '}'.PHP_EOL;
-            }
+        if (preg_match('@(?:/|end)(if|section|form|for)@', $Token->value, $matches)) {
+            switch ($matches[1]) {
+                case "form":
+                    return 'print $Form->close();';
+                    break;
 
-            return $end;
-        } elseif (preg_match('@(?:/|end)(section)@', $Token->value, $matches)) {
-            return '{{ /section }}';
+                case "for":
+                    $end = '$loop["index"]++;' . PHP_EOL;
+                    $end .= '$loop["first"] = false;' . PHP_EOL;
+                    $end .= '}'.PHP_EOL;
+                    $end .= 'if (isset($loop["parent"])) {' . PHP_EOL;
+                    $end .= "\t" . '$loop = $loop["parent"];' . PHP_EOL;
+                    $end .= '}' . PHP_EOL;
+                    $end .= '}' .PHP_EOL;
+                    return $end;
+                    break;
+
+                case "if":
+                    return '}'.PHP_EOL;
+                    break;
+
+                case "section":
+                    return '{{ /section }}';
+                    break;
+            }
         } else {
             // throw Exception
         }
