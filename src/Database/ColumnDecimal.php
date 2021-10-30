f72a79179731814ce71f159f4e1db19b0bcf9ebb
BlackScorp <cccpmik@gmail.com>
2021-10-30 19:57:04 +0200

    Add new Column for Decimal with precision and scale

---
+++ src/Database/ColumnDecimal.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Avolutions\Database;
+
+class ColumnDecimal extends Column
+{
+    public function __construct(
+        string $name,
+        int $precision,
+        int $scale,
+        ?string $length = null,
+        ?string $default = null,
+        string $null = Column::NOT_NULL,
+        bool $primaryKey = false,
+        bool $autoIncrement = false
+    ) {
+        $columnTypeFormat = sprintf('%s(%d,%d)',ColumnType::DECIMAL,$precision,$scale);
+        parent::__construct($name, $columnTypeFormat, $length, $default, $null, $primaryKey, $autoIncrement);
+    }
+
+}
